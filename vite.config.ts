0e120459bbb09c0ea4feedc5a2b752619d94eddc
Shubham-Rathod-Mac <363shubhamrathod@gmail.com>
2025-06-29 22:09:36 +0530

    remote url change dor dev and prod

--- vite.config.ts
+++ vite.config.ts
@@ -2,23 +2,29 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import federation from "@originjs/vite-plugin-federation";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    federation({
-      name: "main-host",
-      remotes: {
-        music_library_ui_remote_components:"http://localhost:4173/assets/remoteEntry.js",
-      },
-      shared: ["react"],
-    }),
-  ],
-  build: {
-    modulePreload: false,
-    target: "esnext",
+export default defineConfig(({ mode }) => {
+  const isDev = mode === "development";
 
-    minify: false,
-    cssCodeSplit: false,
-  },
+  const remotes = {
+    music_library_ui_remote_components: isDev
+      ? "http://localhost:4173/assets/remoteEntry.js"
+      : "https://music-library-ui-remote.vercel.app/assets/remoteEntry.js",
+  };
+
+  return {
+    plugins: [
+      react(),
+      federation({
+        name: "main-host",
+        remotes,
+        shared: ["react"],
+      }),
+    ],
+    build: {
+      modulePreload: false,
+      target: "esnext",
+      minify: false,
+      cssCodeSplit: false,
+    },
+  };
 });
